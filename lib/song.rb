cf204f28a291097057bbd7c6c64486eaf90e9403
Cara Zellner <carajeanine@gmail.com>
2020-10-11 18:46:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,18 @@ class Song
   def self.genre_count
     genre_count = Hash.new(0)
     @@genres.each do |genre| 
-    genre_count[genre] += 1
+      genre_count[genre] += 1
     end
     genre_count
+  end
+  
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each do |artist|
+      artist_count[artist] += 1 
+    end
+    artist_count
+      
   end
   
  
