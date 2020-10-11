8f53bbbd5574495befa6a1f6b2b06c21cabcb315
Cara Zellner <carajeanine@gmail.com>
2020-10-11 18:29:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
 class Song 
   
+  @@count = 0 
+  
   attr_accessor :name :artist :genre
   
   def initialize
     @name = name 
     @artist = artist
     @genre = genre
+    
   end
   
   def self.new 
